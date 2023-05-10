67dd7d365674f8614e345e32e4cdab2f033f9c8a
Dmitry Polyakov <dmitryypolyakov@gmail.com>
2023-05-10 13:38:30 +0500

    Added locators for the order page

--- src/test/java/com/dimqa/OrderPage.java
+++ src/test/java/com/dimqa/OrderPage.java
@@ -1,4 +1,15 @@
 package com.dimqa;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class OrderPage {
+    private WebDriver driver;
+
+    private By firstNameField = By.xpath(".//input[@placeholder = '* Имя']");
+    private By lastNameField = By.xpath(".//input[@placeholder = '* Фамилия']");
+    private By addressField = By.xpath(".//input[@placeholder = '* Адрес: куда привезти заказ']");
+    private By metroField = By.xpath(".//input[@placeholder = '* Станция метро']");
+    private By phoneField = By.xpath(".//input[@placeholder = '* Телефон: на него позвонит курьер']");
+    private By nextButton = By.xpath(".//button[text() = 'Далее']");
 }
