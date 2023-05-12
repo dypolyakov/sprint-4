9d9fd5003f3b1bfc6dc58fd84edc2cf1b06026db
Dmitry Polyakov <dmitryypolyakov@gmail.com>
2023-05-12 21:47:34 +0500

    New locators and methods have been added to the aboutRentPage page. You can now automatically fill out the form in its entirety. New autotest steps have been added to the Main page for the about rent page and assertion has been added.

--- src/test/java/com/dimqa/AboutRentPage.java
+++ src/test/java/com/dimqa/AboutRentPage.java
@@ -1,6 +1,7 @@
 package com.dimqa;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 
 public class AboutRentPage {
@@ -8,11 +9,38 @@ public class AboutRentPage {
 
     private By dateField = By.xpath(".//input[@placeholder = '* Когда привезти самокат']");
     private By rentalPeriod = By.className("Dropdown-root");
+    private By firstRentalPeriod = By.xpath(".//div[(@class = 'Dropdown-option' and text() = 'сутки')]");
     private By commentFiled = By.xpath(".//input[@placeholder = 'Комментарий для курьера']");
     private By orderButton = By.xpath(".//div[@class = 'Order_Buttons__1xGrp']/button[text() = 'Заказать']");
+    private By confirmButton = By.xpath(".//div[@class = 'Order_Modal__YZ-d3']//button[text() = 'Да']");
+
+    private By orderPlaced = By.className("Order_ModalHeader__3FDaJ");
     
 
     public AboutRentPage(WebDriver driver) {
         this.driver = driver;
+    }
+
+    public void setDate() {
+        String date = "13.05.2023";
+        driver.findElement(dateField).sendKeys(date, Keys.ENTER);
+    }
+
+    public void setRentalPeriod() {
+        driver.findElement(rentalPeriod).click();
+        driver.findElement(firstRentalPeriod).click();
+
+    }
+
+    public void clickOrderButton() {
+        driver.findElement(orderButton).click();
+    }
+
+    public void clickConfirmButton() {
+        driver.findElement(confirmButton).click();
+    }
+
+    public String getOrderPlacedText() {
+        return driver.findElement(orderPlaced).getText();
     }
 }
--- src/test/java/com/dimqa/Main.java
+++ src/test/java/com/dimqa/Main.java
@@ -1,15 +1,19 @@
 package com.dimqa;
 
+import org.hamcrest.MatcherAssert;
+import org.junit.After;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.hamcrest.CoreMatchers.startsWith;
 
 public class Main {
     private WebDriver driver;
 
     @Test
     public void orderScooter() {
-        driver = new ChromeDriver();
+        driver = new FirefoxDriver();
         driver.get("https://qa-scooter.praktikum-services.ru/");
 
         HomePage homePage = new HomePage(driver);
@@ -23,5 +27,18 @@ public class Main {
         orderPage.setMetroStation();
         orderPage.setPhoneNumber("+71234567890");
         orderPage.clickNextButton();
+
+        AboutRentPage aboutRentPage = new AboutRentPage(driver);
+        aboutRentPage.setDate();
+        aboutRentPage.setRentalPeriod();
+        aboutRentPage.clickOrderButton();
+        aboutRentPage.clickConfirmButton();
+
+        MatcherAssert.assertThat(aboutRentPage.getOrderPlacedText(), startsWith("Заказ оформлен"));
+    }
+
+    @After
+    public void tearDown() {
+//        driver.quit();
     }
 }
