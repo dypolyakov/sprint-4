89321d711695161f96097e5141db418628b0f257
Dmitry Polyakov <dmitryypolyakov@gmail.com>
2023-05-13 16:34:04 +0500

    Added comments for locators to HomePage.

--- src/test/java/com/dimqa/HomePage.java
+++ src/test/java/com/dimqa/HomePage.java
@@ -6,10 +6,13 @@ import org.openqa.selenium.WebDriver;
 public class HomePage {
     private WebDriver driver;
 
+    // Верхняя кнопка "Заказать"
     private By topOrderButton = By.xpath(".//div[@class = 'Header_Header__214zg']//button[text() = 'Заказать']");
+    // Нижняя кнопка "Заказать"
     private By bottomOrderButton = By.className("Button_Middle__1CSJM");
+    // Кнопка принятия куки "да все привыкли"
     private By cookiesAcceptButton = By.className("App_CookieButton__3cvqF");
-
+    
     public HomePage(WebDriver driver) {
         this.driver = driver;
     }
