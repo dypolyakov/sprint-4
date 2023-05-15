754c7e1ec879af5e14a3072ffd0b0d0371ed2c53
Dmitry Polyakov <dmitryypolyakov@gmail.com>
2023-05-15 18:40:14 +0500

    Added new method clickFirstQuestion()

--- src/test/java/com/dimqa/HomePage.java
+++ src/test/java/com/dimqa/HomePage.java
@@ -1,7 +1,12 @@
 package com.dimqa;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class HomePage {
     private WebDriver driver;
@@ -12,6 +17,10 @@ public class HomePage {
     private By bottomOrderButton = By.className("Button_Middle__1CSJM");
     // Кнопка принятия куки "да все привыкли"
     private By cookiesAcceptButton = By.className("App_CookieButton__3cvqF");
+
+    private By firstQuestion = By.xpath(".//div[@class = 'accordion__item'][1]");
+    private By firstAnswer = By.xpath(".//div[@id='accordion__panel-0']/p");
+
 
     public HomePage(WebDriver driver) {
         this.driver = driver;
@@ -27,5 +36,16 @@ public class HomePage {
 
     public void clickBottomOrderButton() {
         driver.findElement(bottomOrderButton).click();
+    }
+
+    public void clickFirstQuestion() {
+//        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", firstQuestion);
+//        new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.visibilityOfElementLocated(firstQuestion));
+        driver.findElement(firstQuestion).click();
+    }
+
+    public String getFirstAnswerText() {
+        new WebDriverWait(driver, Duration.ofSeconds(3)).until(ExpectedConditions.visibilityOfElementLocated(firstAnswer));
+        return driver.findElement(firstAnswer).getText();
     }
 }
